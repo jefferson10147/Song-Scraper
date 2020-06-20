2030d0d099f2a5cde6a6239ff2ded1d57b3f6853
Jefferson Montilla <47200981+jefferson10147@users.noreply.github.com>
2020-06-20 15:05:57 -0400

    init

---
+++ song_scrapper.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from optparse import OptionParser
+
+HOST = "https://www.musica.com/letras.asp?t2="
+
+def conection (url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        response.encoding = 'utf-8'
+        content = response.text
+        content = content.replace('<br>','\n')
+        soup = BeautifulSoup(content,'html.parser')
+        return soup
+    else:
+        print("No hay respuesta del servidor")
+        exit(0)
+
+parser = OptionParser()
+parser.add_option("-n","--name",action="store",type="string",dest="song_name")
+(options,args) = parser.parse_args()
+song_name = options.song_name.replace(" ","+")
+soup = conection(''.join([HOST,song_name]))
+table = soup.find('table',{'class':'rst'})
+songs_table = table.find_all('tr')[3].find('table')
+songs_desc = songs_table.find_all('td')[1:-2:2]
+
+print('Results for: "',song_name.replace("+"," "),'"')
+i = 1 
+songs_url = []
+for artist in songs_desc:
+    print(i,'.',artist.a.b.text,':',artist.a.get('href'))
+    songs_url.append(artist.a.get('href'))
+    i+=1
+option = int(input('Press your number option:'))
+
+print('\n\n','Lyrics:\n')
+soup2 = conection(songs_url[option-1])
+lyrics = soup2.find('div',{'id':'letra'}).find_all('p')
+for sentence in lyrics:
+    print(sentence.text)

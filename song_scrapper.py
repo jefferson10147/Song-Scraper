82bf61bbc224de20812f82546631e7f7db9de637
Jefferson Montilla <jefferson.montilla98@gmail.com>
2020-07-12 10:03:33 -0400

    adding an enterpoint and fixing some simplifying some code

--- song_scrapper.py
+++ song_scrapper.py
@@ -13,29 +13,30 @@ def conection (url):
         soup = BeautifulSoup(content,'html.parser')
         return soup
     else:
-        print("No hay respuesta del servidor")
+        print("There's not server response")
         exit(0)
 
-parser = OptionParser()
-parser.add_option("-n","--name",action="store",type="string",dest="song_name")
-(options,args) = parser.parse_args()
-song_name = options.song_name.replace(" ","+")
-soup = conection(''.join([HOST,song_name]))
-table = soup.find('table',{'class':'rst'})
-songs_table = table.find_all('tr')[3].find('table')
-songs_desc = songs_table.find_all('td')[1:-2:2]
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-n","--name",action="store",type="string",dest="song_name")
+    (options,args) = parser.parse_args()
+    song_name = options.song_name.replace(" ","+")
+    soup = conection(''.join([HOST,song_name]))
+    table = soup.find('table',{'class':'rst'})
+    songs_table = table.find_all('tr')[3].find('table')
+    songs_desc = songs_table.find_all('td')[1:-2:2]
 
-print('Results for: "',song_name.replace("+"," "),'"')
-i = 1 
-songs_url = []
-for artist in songs_desc:
-    print(i,'.',artist.a.b.text,':',artist.a.get('href'))
-    songs_url.append(artist.a.get('href'))
-    i+=1
-option = int(input('Press your number option:'))
+    print('\n\nResults for: "',song_name.replace("+"," "),'"')
+    i = 1 
+    songs_url = []
+    for artist in songs_desc:
+        print(i,'.',artist.a.b.text,':',artist.a.get('href'))
+        songs_url.append(artist.a.get('href'))
+        i+=1
+    option = int(input('Press your number option:'))
 
-print('\n\n','Lyrics:\n')
-soup2 = conection(songs_url[option-1])
-lyrics = soup2.find('div',{'id':'letra'}).find_all('p')
-for sentence in lyrics:
-    print(sentence.text)
+    print('\n\nLyrics:\n')
+    soup2 = conection(songs_url[option-1])
+    lyrics = soup2.find('div',{'id':'letra'}).find_all('p')
+    for sentence in lyrics:
+        print(sentence.text)
